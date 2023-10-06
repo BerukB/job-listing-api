2ae6648fce90f7de6a20edd3990c15b72d4b6ac1
BerukB <berukberhanua@gmail.com>
2023-10-06 07:23:44 -0700

    added cors

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const Joi = require("joi");
 const dotenv = require("dotenv").config();
 const {errorHandler} = require('./middleware/errorMiddleware');
 const jobs = require("./routes/jobs");
@@ -7,16 +6,19 @@ const userRoutes = require("./routes/userRoutes");
 const userAuthRoutes = require("./routes/userAuthRoutes");
 const dbConnection = require("./config/database");
 const port = process.env.PORT;
+const cors = require('cors')
+
 
 dbConnection();
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 app.use("/api/jobs", jobs);
 app.use("/api/users", userRoutes);
 app.use("/auth/users", userAuthRoutes);
 
 app.use(errorHandler)
+
 
 app.listen(port, () => console.log(`Server started on ${port}`));
 
